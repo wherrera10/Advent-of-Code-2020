8f4f76bc57dfb022aaa41355e87a6fa4628eaa90
Bill <wherrera10@users.noreply.github.com>
2017-10-27 10:40:30 -1000

    Update test1.jl

--- test1.jl
+++ test1.jl
@@ -1,6 +1,9 @@
 
 
+
 println("How many?")
 x = readline()
-r = match(r"\s*\w+[:]*\s*(\d+)\s*\w+[:]*\s*(\d+)", x)
+r = match(r"\D*(\d+)\D*(\d+)", x)
+println(r)
 print("cows $(r.captures[2]), bulls $(r.captures[1]).")
+
