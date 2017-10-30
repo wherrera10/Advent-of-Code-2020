2b7847e6599bdaed279e9f29a020f65f461a5147
Bill <wherrera10@users.noreply.github.com>
2017-10-30 12:17:31 -1000

    Delete test1.jl

--- test1.jl
+++
@@ -1,6 +0,0 @@
-print4(t::Tuple{Int,Int,Int,Int}) = print("$(t[1])$(t[2])$(t[3])$(t[4])")
-
-println("How many?")
-r = match(r"\D*(\d+)\D*(\d+)", readline())
-print("cows $(r.captures[2]), bulls $(r.captures[1]).")
-
