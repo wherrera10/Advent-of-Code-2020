4e668fc8c6a7f715be1ecbc564bbf7a570a884df
Bill <wherrera10@users.noreply.github.com>
2017-10-31 14:58:43 -1000

    Update test8.jl

--- test8.jl
+++ test8.jl
@@ -1,17 +1,23 @@
-# This program expects a source rules file and a text file for 
+# This program expects a source rules file and a text file for
 # translating to be provided as the first and second arguments
 # of the script, eg julia markovrules.jl <rulesfilename> <textfilename>
 
 function getmarkovrules(filename)
+    nonvoid(m) = (m != match(r"x", "y"))
     rules = []
+    lines = []
     open(filename) do rulesfh
         lines = readlines(rulesfh)
     end
     for line in lines
-        if(!ismatch, r"\A#", line))
+        if(!ismatch(r"\A#", line))
             regm = match(r"/(.*?)\s+->\s+(\.?)(.*)/", line)
-            println(regm.captures)
-            push!(rules, (regm.captures[1], regm.captures[2], regm.captures[3]))
+            if(nonvoid(regm) && length(regm.captures) > 0)
+                println(regm.captures)
+                push!(rules, (regm.captures[1], regm.captures[2], regm.captures[3]))
+            else
+                println("No matches for line $line.")
+            end
         end
     end
     rules
@@ -36,10 +42,10 @@ function markovtranslate(filename, rules)
     txt
 end
 
-const rulesfile = ARGV[1]
-const textfile = ARGV[2]
-#const rulesfile = "rules.txt"
-#const textfile = "input.txt"
+#const rulesfile = ARGV[1]
+#const textfile = ARGV[2]
+const rulesfile = "rules.txt"
+const textfile = "input.txt"
 println("Using rules from file $rulesfile, processing text $textfile.\nOutput:\n")
 markovrules = getmarkovrules(rulesfile)
-println(markovtranslate(textfile))
+println(markovtranslate(textfile, markovrules))
