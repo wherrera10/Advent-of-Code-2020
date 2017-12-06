105b70ecb97463fd387f68a54f30c2597d0493b5
Bill <wherrera10@users.noreply.github.com>
2017-12-05 17:49:20 -1000

    Update s.jl

--- s.jl
+++ s.jl
@@ -1,3 +1,24 @@
+ 
+# Knuth's test cases
+s" Euler"       soundex cr type   \ E460
+s" Gauss"       soundex cr type   \ G200
+s" Hilbert"     soundex cr type   \ H416
+s" Knuth"       soundex cr type   \ K530
+s" Lloyd"       soundex cr type   \ L300
+s" Lukasiewicz" soundex cr type   \ L222 (W test)
+s" Ellery"      soundex cr type   \ E460
+s" Ghosh"       soundex cr type   \ G200
+s" Heilbronn"   soundex cr type   \ H416
+s" Kant"        soundex cr type   \ K530
+s" Ladd"        soundex cr type   \ L300
+s" Lissajous"   soundex cr type   \ L222
+ 
+s" Wheaton"   soundex cr type   \ W350
+s" Ashcraft"  soundex cr type   \ A261  (H tests)
+s" Burroughs" soundex cr type   \ B620
+s" Burrows"   soundex cr type   \ B620  (W test) (any Welsh names?)
+s" O'Hara"    soundex cr type   \ O600  (punctuation test)
+
 const char2num = Dict('B'=>1,'F'=>1,'P'=>1,'V'=>1,'C'=>2,'G'=>2,'J'=>2,'K'=>2,
     'Q'=>2,'S'=>2,'X'=>2,'Z'=>2,'D'=>3,'T'=>3,'L'=>4,'M'=>5,'N'=>5,'R'=>6)
 
