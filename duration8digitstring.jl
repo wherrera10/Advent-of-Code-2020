515d7085f4b3c052ed6f4fbe6d2f68bef5e07673
Bill <wherrera10@users.noreply.github.com>
2017-12-20 13:26:51 -1000

    Update duration8digitstring.jl

--- duration8digitstring.jl
+++ duration8digitstring.jl
@@ -1,5 +1,5 @@
 
-function duration8digitstring(x)
+function float2fixedwidth(x,width=8, leftjustified=true)
     if x == 0.0
         return "0.0     "
     end
@@ -10,23 +10,30 @@ function duration8digitstring(x)
     str = @sprintf("%016.7f", x)
     str = replace(str, r"^[0]*([^0].+)", s"\1")
     str = replace(str, r"(.+[^0])[0]*$", s"\1")
-    if length(str) < 8 && str[1] == '.'
+    if length(str) < width && str[1] == '.'
         str = "0" * str
     end
     if neg
         str = "-" * str
     end
     len = length(str)
-    if len > 8
+    if len > width
         decimalplaces = len - search(str, '.')
-        if len - decimalplaces - 1 > 8
+        if len - decimalplaces - 1 > width
             throw("too many significant decimal places on left")
         end
         str = str[1:8]
+    else
+        while length(str) < width
+            if leftjustified
+                str = str * " "
+            else
+                str = " " * str
+            end
+        end
     end
     str
 end
 
-println(duration8digitstring(18500678.92353532))
-
-
+println(float2fixedwidth(18500678.92353532))
+println(float2fixedwidth(1800.0800))
