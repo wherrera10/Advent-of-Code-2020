add638b3f22e4bfbb120dfda4aa4e6cb135e5894
Bill <wherrera10@users.noreply.github.com>
2020-12-25 14:00:30 -1000

    Update advent25.jl

--- advent25.jl
+++ advent25.jl
@@ -1,8 +1,3 @@
-const input = """
-10212254
-12577395
-"""
-
 const (card, door) = 10212254, 12577395
 
 function transform(subject, loopsize)
@@ -19,15 +14,15 @@ function findloopsize(subject, target)
         (v = rem(subject * v, 20201227)) == target && break
         i += 1
     end
-    return i, v
+    return i
 end
 
 pubkey(loopsize) = transform(7, loopsize)
 
 function part1()
-    cloops, v1 = findloopsize(7, card)
-    dloops, v2 = findloopsize(7, door)
-    println("Part 1: ", cloops, " ", dloops, "  ", transform(card, dloops), " ", transform(door, cloops))
+    cloops = findloopsize(7, card)
+    dloops = findloopsize(7, door)
+    println("Part 1: ", cloops, " ", dloops, " answer: ", transform(card, dloops), " ", transform(door, cloops))
 end
 
 part1()
